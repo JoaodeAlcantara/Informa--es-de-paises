2dd5538dc22294655ef50910196b2b1c21460421
JoaodeAlcantara <joao.gab0509@gmail.com>
2024-10-01 22:46:38 -0300

    codigo arrumado

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.querySelector('button').addEventListener('click', async (event) => {
-event.preventDefault();
+    event.preventDefault();
 
     const name = document.querySelector('#input').value;
     const url = `https://restcountries.com/v3.1/name/${name}`;
@@ -9,29 +9,27 @@ event.preventDefault();
     const display = document.querySelector('.complete-info');
     console.log(dados)
 
-    if(!name){
+    if (!name) {
         erro.innerHTML = 'Vázio, digite o nome do país.'
-    } else{
-        erro.innerHTML = ''
-        if(display.style.display === 'none' || display.style.display === ''){
-            display.style.display = 'block'
+    } else {
+        if (dados.status === 404) {
+            erro.innerHTML = 'Pais não encontrado.'
+        } else {
+            erro.innerHTML = ''
+            if (display.style.display === 'none' || display.style.display === '') {
+                display.style.display = 'block'
+            }
+            info({
+                nome: dados[0].name.common,
+                flag: dados[0].flags.png,
+                capital: dados[0].capital,
+                lingua: Object.values(dados[0].languages),
+                populacao: dados[0].population.toLocaleString(),
+                continente: dados[0].continents,
+                maps: dados[0].maps.googleMaps
+            })
         }
     }
-    if(dados.status === 404){
-        erro.innerHTML = 'Pais não encontrado. <br> Tente digitar em ingles!'
-    } else{
-        info({
-            nome: dados[0].name.common,
-            flag: dados[0].flags.png,
-            capital: dados[0].capital,
-            lingua: Object.values(dados[0].languages),
-            populacao: dados[0].population,
-            continente: dados[0].continents,
-            maps: dados[0].maps.googleMaps
-        })
-    }
-    
-
 })
 
 function info(dados) {
@@ -43,9 +41,3 @@ function info(dados) {
     document.querySelector('#cont').innerHTML = `${dados.continente}`
     document.querySelector('#link').href = `${dados.maps}`
 }
-// https://flagcdn.com/w320/br.png -> bandeira
-// name/ common
-// capital
-// languages
-// pupulation
-// region
